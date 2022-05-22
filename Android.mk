5cfd26869beb4f7455e105027e34638dc437188d
Sourajit Karmakar <sourajit.karmakar@gmail.com>
2022-05-22 16:34:57 -0400

    Bromite: Setup for Android build
    Change-Id: If590efb31bc65d11f755adb033c23101ebc063b2

---
+++ Android.mk
@@ -0,0 +1,21 @@
+LOCAL_PATH := $(my-dir)
+
+# Bromite
+include $(CLEAR_VARS)
+LOCAL_MODULE := org.bromite.bromite
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := APPS
+LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_OVERRIDES_PACKAGES := Browser2 QuickSearchBox
+LOCAL_SRC_FILES := app/$(TARGET_ARCH)_ChromePublic.apk
+include $(BUILD_PREBUILT)
+
+# Bromite Webview
+include $(CLEAR_VARS)
+LOCAL_MODULE := org.bromite.webview
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := APPS
+LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_OVERRIDES_PACKAGES := WebView webview
+LOCAL_SRC_FILES := app/$(TARGET_ARCH)_SystemWebView.apk
+include $(BUILD_PREBUILT)
