0fc520279bf924d00528460b445b9000d9939a35
Sourajit Karmakar <sourajit.karmakar@gmail.com>
2022-05-24 20:30:55 -0400

    Bromite: Use Android source friendly paths
    Change-Id: I18c13a0dc0c742c4fc5d32e07450eb0f417cd90e

--- pull_bromite.sh
+++ pull_bromite.sh
@@ -1,11 +1,11 @@
 function pull_bromite() {
-  mkdir -p app/{ChromePublic,SystemWebView}/arm64
+  mkdir -p vendor/bromite/app/{ChromePublic,SystemWebView}/arm64
 
   url_stem="https://github.com/bromite/bromite/releases/download"
   latest_tag=$(curl -s https://api.github.com/repos/bromite/bromite/releases/latest | jq -r '.tag_name')
 
-  wget -q --show-progress ${url_stem}/${latest_tag}/arm64_ChromePublic.apk -O app/arm64_ChromePublic.apk
-  wget -q --show-progress ${url_stem}/${latest_tag}/arm64_SystemWebView.apk -O app/arm64_SystemWebView.apk
+  wget -q --show-progress ${url_stem}/${latest_tag}/arm64_ChromePublic.apk -O vendor/bromite/app/ChromePublic/arm64/arm64_ChromePublic.apk
+  wget -q --show-progress ${url_stem}/${latest_tag}/arm64_SystemWebView.apk -O vendor/bromite/app/SystemWebView/arm64/arm64_SystemWebView.apk
 }
 
 pull_bromite
